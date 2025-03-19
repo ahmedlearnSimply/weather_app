a1639eb3846e9437b88277bb48dd57bbb4e057b6
ahmed adel <ahmedadel123422@gmail.com>
2025-03-19 02:42:53 +0200

    commit

---
+++ lib/screens/home.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarBrightness: Brightness.dark,
+        ),
+      ),
+      body: Stack(
+        children: [],
+      ),
+    );
+  }
+}
