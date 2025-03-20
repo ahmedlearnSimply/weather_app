0d5bf27d0982eab34bbf7036ec60620bd2e0f845
ahmed adel <ahmedadel123422@gmail.com>
2025-03-21 01:23:28 +0200

    commit

---
+++ lib/services/weather_services.dart
@@ -0,0 +1,18 @@
+import 'package:dio/dio.dart';
+import 'package:weather_app/constants/api_constants.dart';
+
+class WeatherServices {
+  Dio dio = new Dio();
+  Future<Map<String, dynamic>> getWeather(String location) async {
+    try {
+      final response = await dio.get(ApiConstants.baseUrl, queryParameters: {
+        'q': location,
+        'key': ApiConstants.apiKey,
+        'aqi': 'no'
+      });
+      return response.data;
+    } catch (error) {
+      throw Exception('Error: $error');
+    }
+  }
+}
